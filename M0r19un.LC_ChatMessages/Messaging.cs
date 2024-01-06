185fc5b5455344e77fd7c59e74dc2f68102521c4
Morigun <GunGrayv@gmail.com>
2024-01-06 12:18:07 +0300

    first commit

---
+++ M0r19un.LC_ChatMessages/Messaging.cs
@@ -0,0 +1,8 @@
+namespace M0r19un.LC_ChatMessages{
+    public class Messaging{
+        public static void Init()
+        {
+            
+        }
+    }
+}
---
+++ M0r19un.LC_ChatMessages/Plugin.cs
@@ -0,0 +1,16 @@
+using M0r19un.LC_Lib;
+
+namespace M0r19un.LC_ChatMessages{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, ModName, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin{
+        public const string ModGUID = PluginInfo.PLUGIN_GUID;
+        public const string ModName = "Message information mod";
+        public const string ModVersion = PluginInfo.PLUGIN_VERSION;
+
+        private void Awake(){
+            M0r19un.LC_Lib.M0r19unLogger.Initialize(ModGUID);
+            M0r19un.LC_Lib.M0r19unLogger.Log($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            
+        }
+    }
+}
