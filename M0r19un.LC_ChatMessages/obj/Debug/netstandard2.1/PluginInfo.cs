fda539f4732a86096d03d1e573c0f0c80293b501
Denis Suvorov <GunGrayv@gmail.com>
2024-01-06 12:35:20 +0300

    Delete M0r19un.LC_ChatMessages/obj directory

--- M0r19un.LC_ChatMessages/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace M0r19un.LC_ChatMessages
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "M0r19un.LC_ChatMessages";
-        public const string PLUGIN_NAME = "M0r19un.LC_ChatMessages";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
